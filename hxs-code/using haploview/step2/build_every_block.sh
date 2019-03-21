80368a415e7b6874501e8c4951a8be68b4c8d7fc
hxs1992 <32355294+woaishiye@users.noreply.github.com>
2019-03-21 17:12:51 +0800

    how many blocks in intergene

--- hxs-code/using haploview/step2/build_every_block.sh
+++ hxs-code/using haploview/step2/build_every_block.sh
@@ -16,7 +16,7 @@ do
 	$PYTHON combine_haploview_result.py $i.txt  intergene_$i.GAMsnp.txt intergene_$i.x.ped.4GAMblocks  intergene_$i.GAMblock.txt
 	$PYTHON combine_haploview_result.py $i.txt  intergene_$i.GABRsnp.txt intergene_$i.x.ped.GABRIELblocks  intergene_$i.GABRblock.txt
 	$PYTHON combine_haploview_result.py $i.txt  intergene_$i.SPIsnp.txt intergene_$i.x.ped.SPINEblocks  intergene_$i.SPIblock.txt
-	for j in $(seq 0 10)
+	for j in $(seq 0 5)#实际计算时一个intergene区域可以最多得到30个block
 	do
 	$PYTHON test.py intergene_$i.GAMblock.txt $j intergene_$i.GAMsnp.txt intergeneGAM_$i.$j.txt intergeneGAM_$i.$j.block
 	$PYTHON test.py intergene_$i.GABRblock.txt $j intergene_$i.GABRsnp.txt intergeneGABR_$i.$j.txt intergeneGABR_$i.$j.block
@@ -26,7 +26,7 @@ done
 
 for i in $(seq 0 6)
 do
-	for j in $(seq 0 10)
+	for j in $(seq 0 5)#实际计算时一个intergene区域可以最多得到30个block
 	do
 	$PYTHON combine_all_block_together.py intergeneGAM_$i.$j.block intergeneGAM_all.block
 	$PYTHON combine_all_block_together.py intergeneGABR_$i.$j.block intergeneGABR_all.block
